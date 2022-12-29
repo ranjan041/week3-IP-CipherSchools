f86144be6b90a401c419c9ace35b15edc5b35d14
Ayush Ranjan <119071463+ranjan041@users.noreply.github.com>
2022-12-29 22:08:50 +0530

    Create element8.js

---
+++ element8.js
@@ -0,0 +1,7 @@
+// Modifying Inline Style in JS
+
+let footer = document.querySelector("footer").style;
+console.log(footer);
+// let footerstyle = (document.querySelector("footer").style.color = "purple");
+let footerstyle = (document.querySelector("footer").style.backgroundColor =
+  "purple");
