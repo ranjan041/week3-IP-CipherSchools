395d223838c7e93854672974a48c0d0eca785d13
Ayush Ranjan <119071463+ranjan041@users.noreply.github.com>
2022-12-29 22:12:44 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,40 @@
+// var (Global scoped variable)
+var container = 13;
+// console.log(container);
+var container = "Blue";
+console.log(container);
+
+var x = 13,
+  y = 18,
+  z = 20;
+console.log(x, y, z);
+
+var contain;
+console.log(contain);
+
+// example
+varbgColor = "green";
+document.querySelector(".up-container").style.bachgroundColor = bgColor;
+var bgColor()="pink";
+document.querySelector(".down-container").style.bachgroundColor = bgColor;
+function heading() {
+  var bgColor = "blue";
+  document.querySelector(".heading").style.color = bgColor;
+}
+heading();
+var bgColor="blue";
+
+// let
+// Blocked Scope
+var bgColor = "green";
+document.querySelector(".up-container").style.bachgroundColor = bgColor;
+// var bgColor()="pink";
+// document.querySelector(".down-container").style.bachgroundColor = bgColor;
+let color="pink"
+
+// const
+// Blocked shaped constant (similar to let)
+const bgColor = "green";
+document.querySelector(".up-container").style.bachgroundColor = bgColor;
+// const bgColor()="pink";
+// document.querySelector(".down-container").style.bachgroundColor = bgColor;
