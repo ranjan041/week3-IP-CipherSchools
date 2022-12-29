c18509523e23fb403d8c4cc339a7a2d3a2df0726
Ayush Ranjan <119071463+ranjan041@users.noreply.github.com>
2022-12-29 22:05:26 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,34 @@
+// string
+let string = "Text";
+console.log("string:", string);
+
+// integer
+let integer = 13;
+console.log("number/integer:", integer);
+
+// float value
+let float = 3.8;
+console.log("float value:", float);
+
+// boolean value
+let boolean = true;
+console.log("Boolean:", boolean);
+
+// null value
+let something = null;
+console.log(something);
+
+// undefined
+let undefinedAssign;
+console.log("Undefined:", undefined);
+
+// object
+const object = {
+  class: "Twelveth",
+  number: 50,
+};
+
+console.log("Object Data", typeof object);
+// array
+const array = ["laptop", "Diary", "Pens", "Coffee", "Mango"];
+console.log("array:", array);
